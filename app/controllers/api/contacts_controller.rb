f205ded6cfe6628eb9894d51c3b162a279c421b7
shane <smp1993@gmail.com>
2019-07-01 21:06:19 -0400

    added restful actions for contacts app

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -1,2 +1,44 @@
 class Api::ContactsController < ApplicationController
+
+  def index
+    @contacts = Contact.all
+    render 'index.json.jb'
+  end
+
+  def show
+    the_id = params[:id]
+    @contact = Contact.find_by(id: the_id)
+    render 'show.json.jb'
+  end
+
+  def create
+    the_id = params[:id]
+    @contact = Contact.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone_number: params[:phone_number]
+    )
+    @contact.save
+    render 'show.json.jb'
+  end
+
+  def update
+    the_id = params[:id]
+    @contact = Contact.find_by(id: the_id)
+    @contact.first_name = params[:first_name]
+    @contact.last_name = params[:last_name]
+    @contact.email = params[:email]
+    @contact.phone_number = params[:phone_number]
+    @contact.save
+    render 'update.json.jb'
+  end
+
+  def destroy
+    the_id = params[:id]
+    contact = Contact.find_by(id: the_id)
+    contact.destroy
+    render 'destroy.json.jb'
+  end
+
 end
